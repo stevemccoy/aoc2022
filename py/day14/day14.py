eb381d476f92f19e4dd2ad70c216614a6dacaf17
Steve McCoy <stephen.a.mccoy@gmail.com>
2023-01-25 10:41:36 +0000

    Python day 14 complete

--- py/day14/day14.py
+++ py/day14/day14.py
@@ -1,4 +1,9 @@
+#
+# Day 14 - Regolith Reservoir.
+#
+
 import numpy as np
+import re
 
 # Globals.
 
@@ -6,9 +11,8 @@ import numpy as np
 num_rows = num_cols = 1000
 grid = np.zeros((num_rows, num_cols), dtype = np.int8)
 
-
 # Read the contents of the given file.
-def process_input(file_name):
+def gulp_input(file_name):
 	with open(file_name, 'r') as f:
 		return list(f)
 
@@ -17,30 +21,31 @@ def draw_line(x1, y1, x2, y2):
 	count = 0
 	if x1 == x2:
 		for y in range(min(y1,y2), max(y1,y2)+1):
-			grid[x1,y] = 1
+			grid[y, x1] = 2
 			count += 1
 	elif y1 == y2:
 		for x in range(min(x1,x2), max(x1,x2)+1):
-			grid[x,y1] = 1
+			grid[y1, x] = 2
 			count += 1
+	else:
+		print('Error in draw_line', (x1, y1, x2, y2))
 	return count
 
 # Mark the positions of the rock formations on the grid.
 def draw_rocks(lines):
 	count = 0
+	pattern = r'(?P<x>\d+),(?P<y>\d+)'
 	for line in lines:
-		x1 = int(line[:line.find(",")])
-		y1 = int(line[line.find(",")+1])
-#		print((x1,y1))
+		d = re.match(pattern, line).groupdict()
+		x1,y1 = int(d['x']),int(d['y'])
 		s = line
 		p = line.find(" ->")
 		while p != -1:
 			s = s[p+4:]
 			e = s.find(" ")
 			ps = s if e == -1 else s[:e] 
-			x2 = int(ps[:ps.find(",")])
-			y2 = int(ps[ps.find(",")+1:])
-#			print((x2,y2))
+			d = re.match(pattern, ps).groupdict()
+			x2,y2 = int(d['x']),int(d['y'])
 			# Draw the line HERE.
 			count += draw_line(x1, y1, x2, y2)
 			x1,y1 = x2,y2
@@ -49,42 +54,81 @@ def draw_rocks(lines):
 
 def drop_sand_grain():
 	x,y = 500,0
+	if grid[y, x] != 0:		# Inlet blocked.
+		return False
 	while y < 999:
-		if grid[x, y + 1] == 0:
+		if grid[y + 1, x] == 0:
 			y += 1
-		elif grid[x - 1, y + 1] == 0:
+		elif grid[y + 1, x - 1] == 0:
 			x += -1
 			y += 1
-		elif grid[x + 1, y + 1] == 0:
+		elif grid[y + 1, x + 1] == 0:
 			x += 1
 			y += 1
 		else:
-			grid[x, y] = 1
+			grid[y, x] = 1
 			return True
 	# Get to here and the sand falls off into the void.
 	return False
 
+def limit_bounds(a):
+	rows,cols = np.nonzero(a)
+	return (min(rows), min(cols), max(rows), max(cols))
+
+def print_grid(g, bounds = None):
+	fy,fx,ty,tx = bounds if bounds else limit_bounds(g)
+	print(f"From: ({fx}, {fy}), To: ({tx}, {ty})")
+	for y in range(fy, ty + 1, 1):
+		s = []
+		for x in range(fx, tx + 1, 1):
+			ch = ' ' if g[y, x] == 0 else '#' if g[y, x] == 2 else 'o'
+			s.append(ch)
+		print("".join(s))
+
 def part1_for(file_name):
 	global grid
 	grid = np.zeros((num_rows, num_cols), dtype = np.int8)
-
-	lines = process_input(file_name)
+	lines = gulp_input(file_name)
 	count = draw_rocks(lines)
 	print('Drew ', count, ' rock cells')
+#	print_grid(grid)
+	grains = 0
+	while drop_sand_grain():
+		grains += 1
+	print('Dropped ', grains, ' sand grains.')
+	drop_sand_grain()
+	print_grid(grid)
+
+def part2_for(file_name):
+	global grid
+	grid = np.zeros((num_rows, num_cols), dtype = np.int8)
+	lines = gulp_input(file_name)
+	count = draw_rocks(lines)
+	fy,fx,ty,tx = limit_bounds(grid)
+	ty += 2
+	draw_rocks([f"0,{ty} -> {num_cols - 1},{ty}"])
+	print('Drew ', count, ' rock cells')
+	print_grid(grid, (fy,fx,ty,tx))
 
 	grains = 0
 	while drop_sand_grain():
 		grains += 1
 
 	print('Dropped ', grains, ' sand grains.')
-
+	print_grid(grid, (fy,fx,ty,tx))
 
 
 # Main processing.
 print('Advent of Code 2022 - Day 14, Part 1.')
 print('Running test...')
-part1_for('day14.txt')
+part1_for('test14.txt')
 
 print('Running full set...')
 part1_for('day14.txt')
 
+print('Part 2.')
+print('Running test...')
+part2_for('test14.txt')
+
+print('Running full set...')
+part2_for('day14.txt')
